3088fdb08ebcfda5d0ea7b6fb6a926d08501485b
(no author) <(no author)@cc6212e4-6f35-474b-bc8b-360f16786c82>
2008-06-14 13:27:17 +0000

    Currently works with gchartrb
    git-svn-id: file:///usr1/svn/southee/Garmin@5 cc6212e4-6f35-474b-bc8b-360f16786c82

---
+++ ruby/gchart.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+#
+
+require 'rubygems'
+require 'google_chart'
+
+require 'parse'
+
+class AltitudeChart
+
+  attr_reader :points,:track
+
+  NumPoints = 100
+  Size = "500x300"
+
+  def initialize(track)
+    d = track.distance
+    delta = track.distance.to_f / NumPoints
+    upto = delta
+    index = 1
+    points = []
+    points << track.first.elev.to_i
+    while upto < track.distance
+      # Find the point after the 'upto' location
+      while track[index].total_distance < upto
+        index += 1
+        last if index >= track.size
+      end
+      last if index >= track.size
+
+      # We need to add a value, the altitude should be taken from between
+      # track[index-1] and track[index]
+      upto += delta
+      points << track[index].elev.to_i
+    end
+
+    @points = points
+    @track = track
+  end
+
+  # The input is an array of arrays, which are [distance, grade, speed]
+  def url
+    min = (@points.min.to_i / 100 * 100)
+    max = ((@points.max.to_i+100) / 100 * 100)
+
+    scale = 4000.0 / (max - min).to_f
+    shifted_points = @points.map { |p| (p - min) * scale }
+#    puts shifted_points
+
+    l = GoogleChart::LineChart.new(Size,@track.name,false) do |gc|
+      gc.data_encoding = :extended
+      gc.max_value(4000)
+      gc.data nil, shifted_points, 'ff0000'
+      gc.axis :x, :range => [0, @track.distance.to_i/1000]
+      gc.axis :y, :range => [min, max]
+#      puts @track.distance
+      x_step = @track.distance.to_i.to_f/1000/10
+      gc.grid :y_step => 100.0/((max - min)/100),
+              :x_step => 100.0/x_step
+#      puts @points.min.to_s + "---" + @points.max.to_s
+    end
+    l.to_url
+  end
+
+end
+
+ARGV.each do |file|
+  tracks = Gpx.new(file) do |track|
+    chart = AltitudeChart.new(track)
+    puts '<img class="gchart" src="' + chart.url + '">"'
+#    system("konqueror '" + chart.url + "'")
+  end
+
+  
+
+end
---
+++ ruby/latitude.rb
@@ -0,0 +1,151 @@
+class Float
+  def to_radians
+    Math::PI * self / 180.0
+  end
+
+  def to_bearing
+    ((self * 180.0 / Math::PI) + 360) % 360
+  end
+end
+
+class GpsPoint
+  require 'time'
+  include Math
+
+  EarthRadius = 6371000.0 # in meters
+
+  attr_accessor :lat,:long, :elev, :time
+
+  def initialize(lat = 0.0,long = 0.0, elev = 0.0, time = nil)
+
+    unless lat.kind_of? Hash
+      @lat = lat.to_f
+      @long = long.to_f
+      @elev = elev.to_f
+      @time = load_time time
+    else
+      @lat =  0.0
+      @long =  0.0
+      @elev =  0.0
+      @time = nil
+      lat.each_pair do |k,v|
+        case k
+        when "Longitude",:longitude
+          @long = v
+        when "Latitude", :latitude
+          @lat = v
+        when "Altitude", :altitude
+          @elev = v
+        when "Time",     :time
+          @time = load_time v
+        end
+      end
+    end
+  end
+
+  # Return the distance in meters between 2 points, calculated using the
+  # Haversine formula, http://www.movable-type.co.uk/scripts/latlong.html
+  def distance_haversine(p)
+    d_lat  = (p.lat  - @lat).to_radians
+    d_long = (p.long - @long).to_radians
+
+    sd_lat  = sin(d_lat  / 2.0)
+    sd_long = sin(d_long / 2.0)
+
+    a = sd_lat * sd_lat +
+        cos(@lat.to_radians) * cos(p.lat.to_radians) * sd_long * sd_long
+    c = 2.0 * atan2(sqrt(a),sqrt(1.0 - a))
+    EarthRadius * c
+  end
+  alias distance distance_haversine
+
+  def distance_spherical(p)
+    o_lat = lat.to_radians
+    p_lat = lat.to_radians
+    d_long = (p.long - @long).to_radians
+
+    d = acos(sin(o_lat) * sin(p_lat) + cos(o_lat) * cos(p_lat) * cos(d_long))
+    d * EarthRadius
+  end
+
+  def elapsed(p)
+    if time && p.time
+      time - p.time
+    else
+      0.0
+    end
+  end
+
+  def bearing(p)
+    d_lat  = (p.lat  - @lat).to_radians
+    d_long = (p.long - @long).to_radians
+
+    o_lat = @lat.to_radians
+    p_lat = p.lat.to_radians
+
+    y = sin(d_long) * cos(p_lat)
+    x = cos(o_lat)  * sin(p_lat) - sin(o_lat) * cos(p_lat) * cos(d_long)
+    atan2(y,x).to_bearing
+  end
+
+  # Grade between the points in degrees.
+  def grade(p)
+    d = distance(p)
+    e = @elev - p.elev
+    (e / d) * 100.0
+  end
+
+  def speed_mps(p)
+    t = elapsed(p)
+    if t == 0.0
+      0.0
+    else
+      (distance(p) / t).abs
+    end
+  end
+
+  def speed_kmh(p)
+    speed_mps(p)*60*60/1000
+  end
+
+  private 
+  def load_time(time)
+    case
+    when time.kind_of?(String)
+      Time.parse time
+    when time.kind_of?(DateTime)
+      time
+    when time.kind_of?(NilClass)
+      nil
+    else
+      raise "unknown class: #{time.class}"
+    end
+  end
+
+end
+
+if $0 == __FILE__
+#  p0 = GpsPoint.new(-27.515666,153.024941,33.7)
+#  p1 = GpsPoint.new(-27.515695,153.024919,22.0)
+
+  p0 = GpsPoint.new(:longitude => 153.024893,
+                      :latitude => -27.514729,
+                      :altitude => 26.9,
+                      :time => "2007-12-07T18:43:10Z")
+  p1 = GpsPoint.new(:longitude => 153.024925,
+                      :latitude => -27.514597,
+                      :altitude => 25.4,
+                      :time => "2007-12-07T18:43:12Z")
+
+    b = p0.bearing(p1)
+    d = p0.distance(p1)
+    # Distance per second
+    s = p0.speed_mps(p1);
+    g = p0.grade(p1)
+
+    printf "Distance = %.2fm\n",d
+    printf "Speed    = %.2f m/h\n", s * 60 * 60
+    printf "Bearing  = %.2f\n", b
+    printf "Grade    = %.2f\n", g
+end
+
