d7b51e1ffea37a7fbc0bc271753b2dc73673f566
Eric Young <eay@pobox.com>
2018-07-28 14:52:33 +1000

    Add a file to rename gpx files to have their date (localtime) on the front.

---
+++ gpx-rename.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'date'
+require 'pathname'
+
+ARGV.each do |filename|
+  fp = Pathname.new(filename)
+  dir = fp.dirname
+  base_name = fp.basename
+
+  if m = base_name.to_s.match(/^(.*?.gpx)(.gpx)+$/)
+    base_name = Pathname.new(m[1]).basename
+    change = true
+  end
+
+  r = open(filename).read
+  if m = r.match(/<time>([^<]+)<\/time>/)
+    date = DateTime.parse(m[1]) + Rational(10,24)
+    local = date.strftime("%Y-%m-%d")
+    localu = date.strftime("%Y_%m_%d")
+    rlocal = date.strftime("%d-%m-%Y")
+    rlocalu = date.strftime("%d_%m_%Y")
+
+    if m = base_name.to_s.match(/((#{local}|#{localu}|#{rlocal}|#{rlocalu})[-_]+)+(.*?)$/)
+      base_name = Pathname.new(m[3])
+      change = true
+    end
+
+    if m = base_name.to_s.match(/^(#{local}-){2,}(.*)$/)
+      base_name = Pathname.new(m[2])
+      change = true
+    end
+
+    new_name = dir + "#{local + '-' + base_name.to_s}"
+   # puts("#{filename} -> #{new_name}") if change
+    File.rename(filename, new_name)
+  end
+end
